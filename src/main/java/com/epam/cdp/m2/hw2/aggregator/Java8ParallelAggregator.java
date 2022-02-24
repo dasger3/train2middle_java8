30369b9ae2924ddc535bb30af42ad5e846378064
Oleksii_Voronin2 <oleksii_voronin2@epam.com>
2022-02-24 02:46:03 +0200

    Finish Java8ParallelAggregator

--- src/main/java/com/epam/cdp/m2/hw2/aggregator/Java8ParallelAggregator.java
+++ src/main/java/com/epam/cdp/m2/hw2/aggregator/Java8ParallelAggregator.java
@@ -1,23 +1,38 @@
 package com.epam.cdp.m2.hw2.aggregator;
 
-import java.util.List;
-
 import javafx.util.Pair;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Java8ParallelAggregator implements Aggregator {
 
     @Override
     public int sum(List<Integer> numbers) {
-        throw new UnsupportedOperationException();
+        return numbers.parallelStream().mapToInt(Integer::valueOf).sum();
     }
 
     @Override
     public List<Pair<String, Long>> getMostFrequentWords(List<String> words, long limit) {
-        throw new UnsupportedOperationException();
+        List<Pair<String, Long>> result = new LinkedList<>();
+        words.parallelStream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .forEach((key, value) -> result.add(new Pair<>(key, value)));
+        return result.parallelStream().limit(limit).collect(Collectors.toList());
     }
 
     @Override
     public List<String> getDuplicates(List<String> words, long limit) {
-        throw new UnsupportedOperationException();
+        List<String> result = new LinkedList<>();
+        words.parallelStream()
+                .collect(Collectors.groupingBy(String::toUpperCase, Collectors.counting()))
+                .entrySet().stream()
+                .filter(n -> n.getValue() > 1).forEach(n -> result.add(n.getKey()));
+        return result.parallelStream().sorted((e1, e2) ->
+                        (e1.length() == e2.length()) ? e1.compareTo(e2) : (e1.length() < e2.length()) ? -1 : 1)
+                .limit(limit)
+                .collect(Collectors.toList());
     }
 }
